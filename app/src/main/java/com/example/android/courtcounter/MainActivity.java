655f783876435270f3de2c1f2f8880323cbe1515
Omodamola <darmhoo@hotmail.com>
2018-01-15 13:52:48 +0100

    activity commit

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,41 +7,110 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int points = 0;
+    int pointA = 0;
+    int pointB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b1 = findViewById(R.id.a_3points);
+        Button a3 = findViewById(R.id.a_3points);
+        a3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pointA += 3;
+                TextView textView = findViewById(R.id.team_a_score);
+                textView.setText("" + pointA);
 
+            }
+        });
+
+        Button a2 = findViewById(R.id.a_2points);
+        a2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pointA += 2;
+                TextView textView = findViewById(R.id.team_a_score);
+                textView.setText("" + pointA);
+
+            }
+        });
+
+        Button a1 = findViewById(R.id.a_free);
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pointA += 1;
+                TextView textView = findViewById(R.id.team_a_score);
+                textView.setText("" + pointA);
+
+            }
+        });
+
+        Button b3 = findViewById(R.id.b_3points);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pointB +=3;
+                TextView textView = findViewById(R.id.team_b_score);
+                textView.setText("" + pointB);
+            }
+        });
+
+        Button b2 = findViewById(R.id.b_2points);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pointB +=2;
+                TextView textView = findViewById(R.id.team_b_score);
+                textView.setText("" + pointB);
+            }
+        });
+
+        Button b1 = findViewById(R.id.b_free);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                points += 3;
-                display(points);
+                pointB +=1;
+                TextView textView = findViewById(R.id.team_b_score);
+                textView.setText("" + pointB);
             }
         });
-        Button b2 = findViewById(R.id.b_3points);
-        b2.setOnClickListener(new View.OnClickListener() {
+        Button reset = findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                points += 3;
-                display(points);
+                pointA = pointB = 0;
+                TextView textView = findViewById(R.id.team_a_score);
+                textView.setText("" + pointA);
+                TextView textView1 = findViewById(R.id.team_b_score);
+                textView1.setText("" + pointB);
             }
         });
-        Button b3 = findViewById(R.id.a_2points);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                points += 2;
-                display(points);
-            }
-        });
+
+
+
+
+
+
+
+
+
+
+
+//
     }
 
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.team_a_score);
+    private void display(int number, int x) {
+        TextView quantityTextView = (TextView) findViewById(x);
         quantityTextView.setText("" + number);
+    }
+    private void setButton(Button b, final int n){
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
